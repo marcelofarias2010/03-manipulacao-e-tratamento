cc42c066739c9fff8de18749d6c2d86449e402db
Marcelo Farias <marcelofarias2010@users.noreply.github.com>
2022-05-27 21:02:26 -0300

    up

--- 03-11-interaco-com-urls/index.php
+++ 03-11-interaco-com-urls/index.php
@@ -1,15 +1,77 @@
 <?php
-require __DIR__ . '/../../fullstackphp/fsphp.php';
+require __DIR__ . '/../fullstackphp/fsphp.php';
 fullStackPHPClassName("03.11 - Interação com URLs");
 
 /*
  * [ argumentos ] ?[&[&][&]]
  */
 fullStackPHPClassSession("argumentos", __LINE__);
+echo "<h1><a  href='index.php'>Clear</a></h1>";
+echo "<h1><a  href='index.php?arg1=true&arg2=true'>Argumentos</a></h1>";
 
+$data = [
+    "name" => "Marcelo",
+    "company" =>" ADAPS",
+    "mail" => "marcelo.farias@adapsbrasil.com.br"
+];
+$arguments = http_build_query($data);
+echo "<p><a  href='index.php?{$arguments}'>Args By Array</a></p>";
+var_dump($_GET);
 
+$object = (object)$data;
+var_dump(
+    $object,
+    http_build_query($object)
+);
 /*
  * [ segurança ] get | strip | filters | validation
  * [ filter list ] https://php.net/manual/en/filter.filters.php
  */
 fullStackPHPClassSession("segurança", __LINE__);
+
+$dataFilter = http_build_query([
+    "name" => "Marcelo",
+    "company" =>" ADAPS",
+    "mail" => "marcelo.farias@adapsbrasil.com.br",
+    "site" => "adapsbrasil.com.br",
+    "script" => "<script>alert('Esse é um javascript')</script>"
+]);
+echo "<p><a href='index.php?{$dataFilter}'>Data Filter</a> </p>";
+
+$dataUrl = filter_input_array(INPUT_GET,FILTER_SANITIZE_STRIPPED);
+if ($dataUrl){
+    if (in_array("",$dataUrl)){
+        echo "<p class='trigger warning'>Faltam dados!</p>";
+    }elseif (empty($dataUrl["mail"])){
+        echo "<p class='trigger warning'>Falta o e-mail!</p>";
+    }elseif (!filter_var($dataUrl["mail"],FILTER_VALIDATE_EMAIL)){
+        echo "<p class='trigger warning'>E-mail inválido!</p>";
+    }else{
+        echo "<p class='trigger accept'>Tudo Certo!</p>";
+    }
+}else{
+    var_dump(false);
+}
+
+var_dump($dataUrl);
+
+$dataFilter = http_build_query([
+    "name" => "Marcelo",
+    "company" =>" ADAPS",
+    "mail" => "marcelo.farias@adapsbrasil.com.br",
+    "site" => "adapsbrasil.com.br",
+    "script" => "<script>alert('Esse é um javascript')</script>"
+]);
+
+parse_str($dataFilter, $arrDataFilter);
+var_dump($arrDataFilter);
+
+$dataPreFilter = [
+    "name" => FILTER_SANITIZE_STRING,
+    "company" => FILTER_SANITIZE_STRING,
+    "mail" => FILTER_VALIDATE_EMAIL,
+    "site" => FILTER_VALIDATE_DOMAIN,
+    "script" => FILTER_SANITIZE_STRING
+];
+
+var_dump(filter_var_array($arrDataFilter, $dataPreFilter));
